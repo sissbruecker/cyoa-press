48cb55c4b165c1a34ed76a7ac23b25780dfe188c
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2015-07-24 21:36:12 +0200

    Added book 'Codexian Saga' Fixed image widths Allow for /post/post-id url format

--- src/get-post.js
+++ src/get-post.js
@@ -6,10 +6,10 @@ var cheerio = require('cheerio');
 
 module.exports = function getPost(part, callback) {
 
-    var postId = part.url.split('#')[1];
+    var postId = getPostId(part.url);
     var postSelector = generatePostSelector(postId);
 
-    request(part.url, function(err, resonse, body) {
+    request(part.url, function (err, resonse, body) {
 
         var $ = cheerio.load(body);
         var postContent = $(postSelector).html();
@@ -20,4 +20,27 @@ module.exports = function getPost(part, callback) {
 
 function generatePostSelector(postId) {
     return 'li#' + postId + ' div.messageContent > article > blockquote';
+}
+
+var HASHED_POST_ID_PATTERN = /.*\/page-[0-9]*#post-[0-9]*/;
+var URL_POST_ID_PATTERN = /.*posts\/[0-9]*/;
+
+function getPostId(url) {
+
+    if (url.match(HASHED_POST_ID_PATTERN)) return getHashedPostId(url);
+    if (url.match(URL_POST_ID_PATTERN)) return getUrlPostId(url);
+
+    return null;
+}
+
+function getHashedPostId(url) {
+    return url.split('#')[1];
+}
+
+function getUrlPostId(url) {
+
+    var parts = url.split('/');
+    var id = parts[parts.length - 1];
+
+    return 'post-' + id;
 }
